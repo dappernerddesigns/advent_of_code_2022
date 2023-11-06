69f6d84ab516f78dc1aaaead4ab4731cea5403e6
Verity Gregory <verity.gregory@gmail.com>
2023-11-06 09:16:44 +0000

    day 5 part one complete

--- __test__/dayFive.test.js
+++ __test__/dayFive.test.js
@@ -4,8 +4,8 @@ const {
   findInstructions,
 } = require("../challenges/dayFive");
 
-describe("Input Converter", () => {
-  test("Takes a string and returns an array containing the stacks and instructions array", () => {
+describe("Stack Converter", () => {
+  test("Takes a string and returns the stacks array", () => {
     const input = `    [D]    
 [N] [C]    
 [Z] [M] [P]
@@ -18,5 +18,56 @@ move 1 from 1 to 2`;
 
     const expected = [["Z", "N"], ["M", "C", "D"], ["P"]];
     expect(findStacks(input)).toEqual(expected);
+  });
+});
+describe("Instructions converter", () => {
+  test("Takes a string and returns an instruction array of objects", () => {
+    const input = `    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2`;
+    const expected = [
+      { move: 1, from: 2, to: 1 },
+      { move: 3, from: 1, to: 3 },
+
+      { move: 2, from: 2, to: 1 },
+
+      { move: 1, from: 1, to: 2 },
+    ];
+    expect(findInstructions(input)).toEqual(expected);
+  });
+});
+describe("Crane Games", () => {
+  test("Moving one crate from column two to column 1 creates correct array", () => {
+    const input = `    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2`;
+    const stacks = findStacks(input);
+    expect(craneGames(stacks, [{ move: 1, from: 2, to: 1 }])).toBe("DCP");
+  });
+  test("Following all instructions, final position is CMZ", () => {
+    const input = `    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2`;
+    const stacks = findStacks(input);
+    const instructions = findInstructions(input);
+    expect(craneGames(stacks, instructions)).toBe("CMZ");
   });
 });
--- challenges/dayFive.js
+++ challenges/dayFive.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const findStacks = (inputStr) => {
   const initialBreak = inputStr.split(/^\s*$/m);
   const rawStacks = initialBreak[0].trimEnd();
@@ -24,8 +25,46 @@ const findStacks = (inputStr) => {
   return finalStackArray;
 };
 
-const findInstructions = (inputStr) => {};
+const findInstructions = (inputStr) => {
+  const initialBreak = inputStr.split(/^\s*$/m);
+  const rawInstructions = initialBreak[1].trimStart();
+  const instrArray = rawInstructions.split("\n");
 
-const craneGames = (stacks, instructions) => {};
+  const instrObjArr = instrArray.map((input) => {
+    const instrObj = {};
+    const moves = input.split(" ");
+    instrObj[moves[0]] = Number(moves[1]);
+    instrObj[moves[2]] = Number(moves[3]);
+    instrObj[moves[4]] = Number(moves[5]);
+    return instrObj;
+  });
+  return instrObjArr;
+};
 
+const craneGames = (stacks, instructions) => {
+  instructions.forEach(({ move, from, to }) => {
+    const currColumn = from - 1;
+    const target = to - 1;
+    const payload = stacks[currColumn].splice(-move, move).reverse();
+
+    for (const crate in payload) {
+      stacks[target].push(payload[crate]);
+    }
+  });
+  let topCrates = "";
+  stacks.forEach((crate) => {
+    topCrates += crate[crate.length - 1];
+  });
+  return topCrates;
+};
+
+const getData = () => {
+  return fs.readFileSync("./inputs/dayFive.txt", "utf8");
+};
+
+const rawInput = getData();
+const stacks = findStacks(rawInput);
+const steps = findInstructions(rawInput);
+const result = craneGames(stacks, steps);
+console.log(result);
 module.exports = { findStacks, craneGames, findInstructions };
